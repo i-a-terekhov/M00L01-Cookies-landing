500ba913671120489b3dcdf9fa95bb4b057a18eb
Terekhov Ivan <for-spam32@yandex.ru>
2026-09-24 15:08:35 +0400

    light version

---
+++ scripts/script.js
@@ -0,0 +1,79 @@
+document.getElementById("main-action-button").onclick = function () {
+    document.getElementById("products").scrollIntoView({ behavior: "smooth" });
+}
+
+const links = document.querySelectorAll(".menu-item > a");
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({ behavior: "smooth" });
+    }
+}
+
+const buttons = document.querySelectorAll(".products-item .button ");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function (event) {
+        document.getElementById("order").scrollIntoView({ behavior: "smooth" });
+
+        const orderInput = document.getElementById("product");
+        const productCard = event.currentTarget.closest(".products-item");
+        const productTitleElement = productCard.querySelector(".products-item-title");
+        if (productTitleElement && orderInput) {
+            orderInput.value = productTitleElement.innerText;
+        }
+    }
+}
+
+const prices = document.getElementsByClassName("products-item-price");
+document.getElementById("change-currency").onclick = function (e) {
+    const currentCurrency = e.target.innerText;
+
+    let newCurrency = "$"
+    let coefficient = 1;
+    if (currentCurrency === "$") {
+        newCurrency = "₽"
+        coefficient = 90
+    } else if (currentCurrency === "₽") {
+        newCurrency = "BYN";
+        coefficient = 3
+    } else if (currentCurrency === 'BYN') {
+        newCurrency = '€';
+        coefficient = 0.9;
+    } else if (currentCurrency === '€') {
+        newCurrency = '¥';
+        coefficient = 6.9;
+    }
+    e.target.innerText = newCurrency;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i].getAttribute("data-base-price") === "бесценно") {
+            continue
+        }
+        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    }
+
+}
+
+
+const product = document.getElementById("product");
+const name = document.getElementById("name");
+const phone = document.getElementById("phone");
+document.getElementById("order-action").onclick = function () {
+    let hasError = false;
+
+    [product, name, phone].forEach((item) => {
+        if (!item.value) {
+            item.style.borderColor = "red";
+            hasError = true;
+        } else {
+            item.style.borderColor = "";
+        }
+    });
+
+    if (!hasError) {
+        [product, name, phone].forEach((item) => {
+            item.value = "";
+        });
+        alert("Спасибо за заказ! Мы скоро свяжемся с вами!")
+    }
+}
+
